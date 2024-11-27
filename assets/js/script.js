469377f779c5fea3a85aaac4002d8d3f7ed885e3
Oxana2332 <far_north@ro.ru>
2024-11-27 18:06:04 +0300

    start corbis

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+	let isOpen = false;
+	function checkIsOpen(newIsOpen) {
+		if (newIsOpen) {
+			$('.header__burger,.header__menu').addClass('active');
+			$('body').addClass('lock');
+		} else {
+			$('.header__burger,.header__menu').removeClass('active');
+			$('body').removeClass('lock');
+		}
+	}
+
+	$('.header__link').click(function (event) {
+		event.preventDefault();
+		const targetPage = event.target.href.split('/').at(-1).split('#')[0];
+		const currentPage = window.location.pathname
+			.split('/')
+			.at(-1)
+			.split('#')[0];
+		if (targetPage === currentPage) {
+			isOpen = false;
+			checkIsOpen(isOpen);
+		}
+		window.location.replace(event.target.href);
+	});
+	$('.header__burger').click(function (event) {
+		isOpen = !isOpen;
+		checkIsOpen(isOpen);
+	});
+});
+
+//слайдер
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	margin: 10,
+	nav: true,
+	responsive: {
+		0: {
+			items: 3,
+		},
+		600: {
+			items: 4,
+		},
+		1000: {
+			items: 8,
+		},
+	},
+});
+
+// const d = new Date();
+// let year = d.getFullYear();
+// document.getElementById('demo').innerHTML = year;
